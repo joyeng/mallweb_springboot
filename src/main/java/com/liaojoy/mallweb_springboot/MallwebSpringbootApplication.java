531bc2f8cf8ad8c278fc87f7ee155bc697d2ac7e
joyeng <joycat30@gmail.com>
2024-09-28 17:45:53 +0800

    初始化 Spring Boot project

---
+++ src/main/java/com/liaojoy/mallweb_springboot/MallwebSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.liaojoy.mallweb_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallwebSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallwebSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liaojoy/mallweb_springboot/MallwebSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liaojoy.mallweb_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallwebSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
