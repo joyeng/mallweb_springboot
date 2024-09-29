82c0ce92217c7c9bd2afccb836a7c3bd4b904bea
joyeng <joycat30@gmail.com>
2024-09-29 16:15:42 +0800

    商品功能 - 實作查詢商品列表

--- src/main/java/com/liaojoy/mallweb_springboot/dao/ProductDao.java
+++ src/main/java/com/liaojoy/mallweb_springboot/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.liaojoy.mallweb_springboot.dao;
 import com.liaojoy.mallweb_springboot.dto.ProductRequest;
 import com.liaojoy.mallweb_springboot.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
--- src/main/java/com/liaojoy/mallweb_springboot/service/ProductService.java
+++ src/main/java/com/liaojoy/mallweb_springboot/service/ProductService.java
@@ -3,7 +3,11 @@ package com.liaojoy.mallweb_springboot.service;
 import com.liaojoy.mallweb_springboot.dto.ProductRequest;
 import com.liaojoy.mallweb_springboot.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
