e43ecd4fe65a4e6419463b31d4cd53ab0d7bc397
joyeng <joycat30@gmail.com>
2024-09-30 23:11:26 +0800

    帳號功能 - 登入

--- src/main/java/com/liaojoy/mallweb_springboot/controller/UserController.java
+++ src/main/java/com/liaojoy/mallweb_springboot/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.liaojoy.mallweb_springboot.controller;
 
+import com.liaojoy.mallweb_springboot.dto.UserLoginRequest;
 import com.liaojoy.mallweb_springboot.dto.UserRegisterRequest;
 import com.liaojoy.mallweb_springboot.model.User;
 import com.liaojoy.mallweb_springboot.service.UserService;
@@ -27,5 +28,12 @@ public class UserController {
 
         //將數據回傳給前端
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @PostMapping("/users/login")
+    public ResponseEntity<User> login(@RequestBody @Valid UserLoginRequest userLoginRequest) {
+        User user = userService.login(userLoginRequest);
+
+        return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 }
--- src/main/java/com/liaojoy/mallweb_springboot/service/UserService.java
+++ src/main/java/com/liaojoy/mallweb_springboot/service/UserService.java
@@ -1,11 +1,15 @@
 package com.liaojoy.mallweb_springboot.service;
 
+import com.liaojoy.mallweb_springboot.dto.UserLoginRequest;
 import com.liaojoy.mallweb_springboot.dto.UserRegisterRequest;
 import com.liaojoy.mallweb_springboot.model.User;
 
 public interface UserService {
 
     User getUserById(Integer userId);
+
     Integer register(UserRegisterRequest userRegisterRequest);
+
+    User login(UserLoginRequest userLoginRequest);
 
 }
